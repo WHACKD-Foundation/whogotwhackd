6644725d0f16d14c179124eabe47c6f01a97634d
Kevin Aleman <kevin@kgaleman.com>
2021-06-28 23:26:21 -0700

    Update etherscan.ts
    Lint errors broke the build.

--- code/src/components/providers/etherscan.ts
+++ code/src/components/providers/etherscan.ts
@@ -5,7 +5,6 @@ import { BigNumber, providers } from 'ethers'
 
 const WHACKD_CONTRACT = '0xCF8335727B776d190f9D15a54E6B9B9348439eEE'
 const ETHERSCAN_APIKEY = 'P5YRK927CQTPU1H1Q2S1FDS117JQR3CEKP'
-const BURN_ADDRESS = '0x0000000000000000000000000000000000000000'
 let provider = new providers.EtherscanProvider(undefined, ETHERSCAN_APIKEY)
 
 export const whackdContract = new Contract(
@@ -21,7 +20,7 @@ export function getWhackdCounter(callback: Function) {
       let whackdCounter = BigNumber.from(result)
       callback(whackdCounter.toString())
     })
-    .catch(function (error) {
+    .catch(function () {
       // TODO: We should do something here, I'm not sure how often this happens
       console.log('Error when updating counter')
     })
